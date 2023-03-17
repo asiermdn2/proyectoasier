f3ca03cd20f4cf856db4ca91f2e7fa8be85c4f2d
asiermdn2 <126161860+asiermdn2@users.noreply.github.com>
2023-03-17 03:07:09 +0100

    Update calculadora.java

--- src/main/java/calculadora.java
+++ src/main/java/calculadora.java
@@ -8,7 +8,7 @@
 
 
 import java.util.Scanner;
-public class calculadora {
+public class calculador {
     static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         int num1 = getInt();
